08be0c58e50f100a2b0ba60ec8398315c4ee4914
Akulkumar Patel <avp76@njit.edu>
2021-02-27 00:17:49 +0000

    added Login.js forgotted to add

---
+++ src/Login.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import logo from './logo.svg';
+import './App.css';
+import { App } from './App.js';
+import io from 'socket.io-client';
+
+const socket = io(); // Connects to socket connection
+
+function handleSubmit(event) {
+    let userName = document.getElementById("join_room");
+   
+    // TODO- send the address on. a socket to the server
+    socket.emit('join_room', {'username': userName.value});
+    
+    
+    console.log('Sent the address ' + userName.value + ' to server!');
+    userName.value = ''
+    ReactDOM.render(<App />, document.getElementById('root'));
+    event.preventDefault();
+}
+
+export function Login() {
+  // body...
+  return(
+    <>
+    <div class='toppane'>
+        <h1>My Chat App</h1>
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label>Enter your username:</label>
+                <input id="join_room" placeholder="Enter Username!" name="username"></input>
+            </div>
+            <button type="submit">Submit</button>
+        </form>
+    </div>
+    </>
+  );
+}
+
+export default Login;
